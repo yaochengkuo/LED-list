8f2779c21aa64463aee9e2e1a28020c6b7a661e7
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2022-12-01 16:43:33 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
-basic.forever(function () {
-	
-})
+let list = [
+[
+0,
+0,
+1,
+0,
+0
+],
+[
+0,
+1,
+1,
+1,
+0
+],
+[
+1,
+1,
+1,
+1,
+1
+],
+[
+0,
+0,
+1,
+0,
+0
+],
+[
+0,
+0,
+1,
+0,
+0
+]
+]
+for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x <= 4; x++) {
+        if (list[y][x] == 1) {
+            led.plot(x, y)
+        } else {
+            led.unplot(x, y)
+        }
+    }
+}
